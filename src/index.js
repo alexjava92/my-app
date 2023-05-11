d499c56d3ff77be78562ddf696536741176077d0
alexjava92 <autoexpert37@yandex.ru>
2023-05-11 23:09:22 +0300

    init 41

--- src/index.js
+++ src/index.js
@@ -13,8 +13,6 @@ let rerenderEntireTree = (state) =>{
             <App state={state}
                  dispatch={store.dispatch.bind(store)}
                  store={store}
-
-
             />
         </React.StrictMode>
     );}
